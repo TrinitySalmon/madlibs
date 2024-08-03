4d7490045e74995ef9fda8bc81652fcbd69c9976
Trinity <salmontrinity2@gmail.com>
2024-08-03 17:42:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+// Define word variables
+let noun = "dragon";
+let verb = "soar";
+let adjective = "mysterious";
+
+// Define numeric value
+let number = 7;
+
+// Calculate a new number using an expression
+let calculatedNumber = number + 3;
+
+// Define a true/false value
+let isEpic = true;
+
+// Get a value entered by the user
+let userName = prompt("Enter your name:");
+
+// Create a short story using the variables
+let story = `In a ${adjective} land, ${userName} encountered a ${noun} that could ${verb} across the skies. 
+The creature traveled ${calculatedNumber} miles, ${number} of which were over mountains. 
+Was it an epic adventure? ${isEpic}!`;
+
+// Output the story to the console
+console.log(story);
 
